637ab6e5a6482830204cd31f42b98b32c047720c
pallavipalechar <pallu12palechar@gmail.com>
2026-06-25 20:59:10 -0500

    New

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: "https://ironguardchat1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
